bffbe90882f7bc04903914628fe4bfeed678b8b5
ihsn525 <ihsansiju456@gmail.com>
2026-02-06 18:38:06 +0530

    Update firebase config to use environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,18 @@
+// src/firebase.js
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC2w5ta6HIIb1UKISWjIlOiuyR68JOCi-A",
-  authDomain: "trendstore-e62ad.firebaseapp.com",
-  projectId: "trendstore-e62ad",
-  storageBucket: "trendstore-e62ad.firebasestorage.app",
-  messagingSenderId: "982703346913",
-  appId: "1:982703346913:web:1c37be4dedbf61d91553a0"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
-export const db = getFirestore(app);
